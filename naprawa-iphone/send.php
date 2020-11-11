dcd94552de3fe70e1afb632826feabf3f8d0d23a
olekrumian <58187457+olekrumian@users.noreply.github.com>
2020-11-11 14:27:00 +0100

    wensday

---
+++ naprawa-iphone/send.php
@@ -0,0 +1,19 @@
+<?
+        $to = "hello@dkolosov.com";
+        $subject = 'Naprawa'; //Заголовок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Imię: <b>'.$_POST['name'].'</b></p>
+                        <p>Numer kontaktowy: <b>'.$_POST['number'].'</b></p>
+                        <p>Model telefonu: <b>'.$_POST['model'].'</b></p>                                 
+                        <p>Сo się stało z urządzeniem: <b>'.$_POST['text'].'</b></p>                                 
+                    </body>
+                </html>'; //Текст сообщения
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: iPro Serwis\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+?>
